cc72d056c21522e073829969beb30a8be2fdd6cb
anis_agwan <anishagwan@gmail.com>
2020-06-25 04:15:29 +0530

    Lec 23: searchField State

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {CardList} from './components/card-list/card-list.component';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      string: "Hello, Friend"
-    }
+      monsters: [],
+      searchField: ''
+    };
   }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.string}</p>
-          <button onClick={() => this.setState({ string: "Hello, Elliot"})}>Change Text</button>
-        </header>
+        <input type='search' placeholder='Search Monsters' onChange={e => this.setState({ searchField: e.target.value})} />
+        <CardList monsters={this.state.monsters}>
+        </CardList>
       </div>
     );
   }

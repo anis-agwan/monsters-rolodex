b6ff8ef74795bc6c5f891bda428e24021c4f4426
anis_agwan <anishagwan@gmail.com>
2020-07-30 01:18:36 +0530

    Updates

---
+++ precache-manifest.82bb9ca661f2ee4cf0e4c7568843f762.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6ad266a31fd52bd1a7890afcb913428",
+    "url": "/anis-agwan/monsters-rolodex.git/index.html"
+  },
+  {
+    "revision": "c230aec924b981fa700d",
+    "url": "/anis-agwan/monsters-rolodex.git/static/css/main.196e7711.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/anis-agwan/monsters-rolodex.git/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/anis-agwan/monsters-rolodex.git/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c230aec924b981fa700d",
+    "url": "/anis-agwan/monsters-rolodex.git/static/js/main.2aa0baa1.chunk.js"
+  },
+  {
+    "revision": "a273a1e263c4f3ee7d20",
+    "url": "/anis-agwan/monsters-rolodex.git/static/js/runtime-main.064e75f1.js"
+  }
+]);
